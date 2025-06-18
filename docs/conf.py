6d3f11dfb77a5905ee156cf2ef7ca28fbdc942a0
cvbngpooran <cvbngpooran@outlook.com>
2025-06-18 15:58:13 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,10 @@
+project = 'Activate Sling Tv'
+author = 'Your Name'
+release = '1.0'
 
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
